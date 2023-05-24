3c47e3ad3dcc8b962a45a304bbf42cc725bcc439
ilselohr <ilse@ostec.de>
2023-05-24 16:53:54 +0200

    add new page and layout files for dynamic routes

---
+++ src/routes/[category]/+layout.js
@@ -0,0 +1,5 @@
+export const load = ({ params:{ category} }) => {
+    return {
+        category
+    }
+}
---
+++ src/routes/[category]/[component]/+page.js
@@ -0,0 +1,5 @@
+export const load = ({ params:{ category, component} }) => {
+    return {
+        category, component
+    }
+}
