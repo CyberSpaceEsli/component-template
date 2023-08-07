a29ca31c32c9784c17eb5aec69727463eb5ed32e
ilselohr <ilse@ostec.de>
2023-08-07 17:27:27 +0200

    wrote documentation content for interface

--- src/components/InterfaceDocumentation.svelte
+++ src/components/InterfaceDocumentation.svelte
@@ -1,9 +1,125 @@
 <div class="mt-5">
    <div class="flex space-x-2 items-center">
-      <svg class="w-8 h-8 text-gray-300" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+      <svg class="w-8 h-8 text-gray-400" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
          <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75" />
       </svg>
-      <h1 class="text-indigo-600 font-bold text-2xl">Documentation about the component-template</h1>
+      <h1 class="text-indigo-600 font-bold text-2xl">UI-Component-Template | Documentation </h1>
    </div>
-   <p>Alert: Be familiar with the best practices and the structured process of Atomic Design</p>
+   <p class="mt-2 text-gray-900 text-sm">Alert: Be familiar with the best practices and the structured process of Atomic Design before your read this documentation.</p>
+
+   <div class="mt-5 py-5 px-9 border">
+      <div class="mt-2 flex space-x-2 items-center">
+         <svg class="w-6 h-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M10.05 4.575a1.575 1.575 0 10-3.15 0v3m3.15-3v-1.5a1.575 1.575 0 013.15 0v1.5m-3.15 0l.075 5.925m3.075.75V4.575m0 0a1.575 1.575 0 013.15 0V15M6.9 7.575a1.575 1.575 0 10-3.15 0v8.175a6.75 6.75 0 006.75 6.75h2.018a5.25 5.25 0 003.712-1.538l1.732-1.732a5.25 5.25 0 001.538-3.712l.003-2.024a.668.668 0 01.198-.471 1.575 1.575 0 10-2.228-2.228 3.818 3.818 0 00-1.12 2.687M6.9 7.575V12m6.27 4.318A4.49 4.49 0 0116.35 15m.002 0h-.002" />
+         </svg>
+         <h2 class="text-indigo-600 text-xl font-bold">What is a component-template?</h2>
+      </div>
+      <div class="mt-2 py-2 px-5 text-gray-900">
+         <p>The component-template delivers a <i>practical</i> and <i>structured</i> process of how to deal with your <b>components</b> in your <b>frontend</b>. </p>
+      </div>
+
+      <!-- Alert component -->
+      <div class="mt-2 rounded-md bg-gray-50 w-max p-5">
+         <div class="flex">
+            <div class="flex-shrink-0">
+               <svg class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                  <path fill-rule="evenodd" d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 2.495zM10 5a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 5zm0 9a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
+               </svg>
+            </div>
+            <div class="ml-3">
+               <h3 class="text-sm font-semibold text-gray-900">Alert:</h3>
+               <div class="mt-2 text-sm text-gray-700">
+                  <ul role="list" class="list-disc space-y-1 pl-5">
+                     <li>Until now this component-template is only compatible with the technologies <b>Svelte</b> and <b>Tailwind CSS<b/>.</li>
+                     <li>It needs to follow the fix structure of the <b>Metholody Atomic Design</b>.</li>
+                  </ul>
+               </div>
+            </div>
+         </div>
+      </div>
+
+      <div class="mt-10 flex space-x-2 items-center">
+         <svg class="w-6 h-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z" />
+         </svg>
+         <h2 class="text-indigo-600 text-xl font-bold">Why use the component-template?</h2>
+      </div>
+      <div class="ml-3">
+         <div class="mt-2 text-sm text-gray-700 py-2 px-5">
+            <ul role="list" class="list-disc space-y-1 pl-5">
+               <li>Maximaze the reuse of your established components and write less code</li>
+               <li>Shared language and process with components because of fixed rules of the categories for components</li>
+               <li>Provides a modern and manageable frontend to show the visual side of your components</li>
+               <li>Faster component tracking with the implemented search tool in the frontend</li>
+            </ul>
+         </div>
+      </div>
+
+      <div class="mt-10 flex space-x-2 items-center">
+         <svg class="w-6 h-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75a4.5 4.5 0 01-4.884 4.484c-1.076-.091-2.264.071-2.95.904l-7.152 8.684a2.548 2.548 0 11-3.586-3.586l8.684-7.152c.833-.686.995-1.874.904-2.95a4.5 4.5 0 016.336-4.486l-3.276 3.276a3.004 3.004 0 002.25 2.25l3.276-3.276c.256.565.398 1.192.398 1.852z" />
+            <path stroke-linecap="round" stroke-linejoin="round" d="M4.867 19.125h.008v.008h-.008v-.008z" />
+         </svg>
+         <h2 class="text-indigo-600 text-xl font-bold">How to install the component-template?</h2>
+      </div>
+      <div class="mt-2 py-2 px-5 text-gray-900">
+         <p>Download the project component-template from the <a href="/" class="text-indigo-700 hover:underline">[npm Registry]</a>. (link?)</p>
+      </div>
+      <div class="mt-2 rounded-md bg-gray-50 w-max p-5">
+            <div class="ml-3">
+               <div class="mt-2 text-sm text-gray-700">
+                     <p><b>$</b> npm i component-template</p>
+               </div>
+            </div>
+      </div>
+      <div class="mt-2 py-2 px-5 text-gray-900">
+         <p>Beneath the <i>lib</i> folder make a folder <b>component-template</b> that contains further folders with the Atomic Design categories. This listed structure is shown below. </p>
+      </div>
+
+      <div class="mt-2 rounded-md bg-gray-50 w-max p-5">
+         <div class="ml-3">
+            <div class="mt-2 text-sm text-gray-700">
+               <p>* lib <br>
+                  ** component-template <br>
+                  *** atoms <br>
+                  *** molecules <br>
+                  *** organism <br>
+                  *** templates <br>
+                  *** pages</p>
+            </div>
+         </div>
+      </div>
+      <div class="mt-2 py-2 px-5 text-gray-900">
+         <p>After that insert your build components into the fitting category of the Atomic Design Methodology. </p>
+      </div>
+
+      <div class="mt-10 flex space-x-2 items-center">
+         <svg class="w-6 h-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+         </svg>
+
+         <h2 class="text-indigo-600 text-xl font-bold">How to use the component-template?</h2>
+      </div>
+      <div class="ml-3">
+         <div class="mt-2 text-sm text-gray-700 py-2 px-5">
+               <p> <b>1</b> Each component lies in its own svelte file</p>
+               <p> <b>2</b> Choose suitable Naming of your components</p>
+               <p> <b>3</b> Reuse your components with the import statement</p>
+               <p> <b>4</b> Pass props or use each iterations for the usage of dynamic data with your components</p>
+         </div>
+      </div>
+
+      <div class="mt-10 flex space-x-2 items-center">
+         <svg class="w-6 h-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
+         </svg>
+         <h2 class="text-indigo-600 text-xl font-bold">How can the component-template help different roles?</h2>
+      </div>
+      <div class="mt-2 py-2 px-5 text-gray-900">
+         <p><b>Design:</b> If a custom component needs to be made the previous design can be used as basis.</p>
+         <p>Content Creation: When creating new content it can be better adapted to the scale of the component. </p>
+         <p>Sales: </p>
+      </div>
+
+   </div>
 </div>
