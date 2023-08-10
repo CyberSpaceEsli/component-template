7fe9a4764dc0fa793c9441af102b2084784271a3
ilselohr <ilse@ostec.de>
2023-08-10 11:53:56 +0200

    removed localhost at start of base url

--- src/functions/link.js
+++ src/functions/link.js
@@ -1,4 +1,7 @@
 // gerneriert relative url aus den categories und components
+
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL || 'localhost:5173';
+
 export function generateLink (componentName, categoryName) {
-    return `http://localhost:5173/${categoryName}/${componentName}`;
+    return `/${categoryName}/${componentName}`;
 }
